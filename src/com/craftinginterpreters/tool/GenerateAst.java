59c6f595aa788c794fbd058bed5cf81d24d34175
daju233 <2212509690@qq.com>
2024-12-13 00:25:39 +0800

    完成控制流(if,else,for,while)

--- src/com/craftinginterpreters/tool/GenerateAst.java
+++ src/com/craftinginterpreters/tool/GenerateAst.java
@@ -13,8 +13,8 @@ public class GenerateAst {
         }
         String outputDir = args[0];
         //生成语法树，定义accept方法供Expr访问来决定如何解析语句
-        defineAst(outputDir, "Expr", Arrays.asList("Assign : Token name, Expr value","Binary : Expr left, Token operator, Expr right", "Grouping : Expr expression", "Literal : Object value", "Unary : Token operator, Expr right","Variable : Token name"));
-        defineAst(outputDir, "Stmt", Arrays.asList( "Block : List<Stmt> statements","Expression : Expr expression", "Print : Expr expression","Var : Token name, Expr initializer"));
+        defineAst(outputDir, "Expr", Arrays.asList("Assign : Token name, Expr value", "Binary : Expr left, Token operator, Expr right", "Grouping : Expr expression", "Literal : Object value", "Logical : Expr left, Token operator, Expr right", "Unary : Token operator, Expr right", "Variable : Token name"));
+        defineAst(outputDir, "Stmt", Arrays.asList("Block : List<Stmt> statements", "Expression : Expr expression", "If : Expr condition, Stmt thenBranch," + " Stmt elseBranch", "Print : Expr expression", "Var : Token name, Expr initializer", "While : Expr condition, Stmt body"));
     }
 
     private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
@@ -38,7 +38,8 @@ public class GenerateAst {
         writer.println("}");
         writer.close();
     }
-    private static void defineType( PrintWriter writer, String baseName, String className, String fieldList) {
+
+    private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
         writer.println(" static class " + className + " extends " + baseName + " {");
 
         // Constructor.
